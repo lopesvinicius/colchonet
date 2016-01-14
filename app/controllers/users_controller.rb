24581b9174318c6a4c864073afa7569534bfaf84
Vinicius <viniciuslopeslps@gmail.com>
2016-01-14 21:48:24 -0200

    estilo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
 
+	def index
+		@users = User.all
+	end
+
 	def show
 		@user = User.find(params[:id])
 	end
@@ -32,7 +36,7 @@ class UsersController < ApplicationController
 			render action: :edit		
 		end	
 	end
-	
+
 	private
 	def user_params
 		params.
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,9 @@ class User < ActiveRecord::Base
 	EMAIL_REGEX = /\A[^@]+@([^@\.]+\.)+[^@\.]+\Z/
 
 	validates_format_of :email, with: EMAIL_REGEX
-	validates_presence_of :email, :full_name, :location, :password
-	validates_confirmation_of :password
+	validates_presence_of :email, :full_name, :location
 	validates_length_of :bio, minimum: 30, allow_blank: false
 	validates_uniqueness_of :email
+
+	has_secure_password
 end
